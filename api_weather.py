59e7516492cb8505dd48f5f5216e10321b862a57
Summer <gamits@student.douglascollege.ca>
2022-11-11 20:50:59 -0800

    fixed bug in weather api

--- api_weather.py
+++ api_weather.py
@@ -1,37 +1,38 @@
 import requests
 from models.model import WeatherForecast
 
+
 def get_weather():
     city_name = "Vancouver,CA"
     api_key = '26e3ab58c982deb6571267759cd689e1'
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={api_key}"
-     
+
     response = requests.get(url).json()["list"]
-    
-    #getting only one weather info per day
+
+    # getting only one weather info per day
     response_list = []
 
     counter_start = 0
     counter_end = 6
 
-    for x in range(0,5):
+    for x in range(0, 5):
 
         temp = 0
         temp_array = []
         temp_max = response[counter_start]["main"]["temp_max"] - 273.15
         temp_min = response[counter_start]["main"]["temp_min"] - 273.15
         dt = ""
-        response_raw =response[0]["main"]
-        
-        for x in range(counter_start,counter_end):
+        response_raw = response[0]["main"]
+
+        for x in range(counter_start, counter_end):
             response_raw = response[x]["main"]
-                #converting temperature from kelvin to celsius
+            # converting temperature from kelvin to celsius
             min = response_raw["temp_min"] - 273.15
             max = response_raw["temp_max"] - 273.15
             temp_array.append(response_raw["temp"] - 273.15)
             if min < temp_min:
                 temp_min = min
-            
+
             if max > temp_max:
                 temp_max = max
 
@@ -39,20 +40,16 @@ def get_weather():
 
         temp_sum = 0
         for x in temp_array:
-            temp_sum+=0
+            temp_sum += x
         temp = temp_sum/8
-        response_raw["temp"] = int(round(temp,0))
-        response_raw["temp_min"] = int(round(temp_min,0))
-        response_raw["temp_max"] = int(round(temp_max,0))
+        response_raw["temp"] = int(round(temp, 0))
+        response_raw["temp_min"] = int(round(temp_min, 0))
+        response_raw["temp_max"] = int(round(temp_max, 0))
         response_raw["dt_txt"] = dt
 
         response_list.append(WeatherForecast(**response_raw))
 
-        counter_start +=7
-        counter_end +=7
+        counter_start += 7
+        counter_end += 7
 
-    
     return response_list
-    
-
-    
