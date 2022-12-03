cb6f31c14f416fb13e086f6c8a5a9cee727b26a1
ferryalvianto <ferryalvianto@gmail.com>
2022-12-02 19:45:23 -0800

    commit

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -5,7 +5,6 @@ from models.timeseries import save_timeseries_to_db
 from models.ml_model_regression import save_model_to_db
 from typing import List
 
-
 # @shared_task(bind=True,autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
 #              name='universities:get_all_universities_task')
 # def get_all_universities_task(self, countries: List[str]):
@@ -22,20 +21,20 @@ from typing import List
 #     return university
 
 def read_oa_csv():
-    oa1 = pd.read_pickle('https://drive.google.com/uc?id=1l7qRdYza9LF80UZrKDJ7ucmdNYMBPvbB', compression='gzip')
-    oa2= pd.read_pickle('https://drive.google.com/uc?id=1jNfSca67sEvvkLkZkUEtgRxcwHEmbjTC', compression='gzip')
-    oa3 = pd.read_pickle('https://drive.google.com/uc?id=1Qfm7iv_-msBqE8xs_gt-2UBwWndheOxi', compression='gzip')
-    oa4 = pd.read_pickle('https://drive.google.com/uc?id=1BGT8fjo-bD4trGOGBfZk7S3DCtp9z-sb', compression='gzip')
+    oa1 = pd.read_pickle('../pkls/OA19.pkl', compression='gzip')
+    oa2= pd.read_pickle('../pkls/OA20.pkl', compression='gzip')
+    oa3 = pd.read_pickle('../pkls/OA21.pkl', compression='gzip')
+    oa4 = pd.read_pickle('../pkls/OA22.pkl', compression='gzip')
     oa = pd.concat([oa1, oa2], ignore_index=True, axis=0)
     oa = pd.concat([oa, oa3], ignore_index=True, axis=0)
     oa = pd.concat([oa, oa4], ignore_index=True, axis=0)
     return oa
 
 def read_cy_csv():
-    cy1 = pd.read_pickle('https://drive.google.com/uc?id=1Ujp7hbfnP5nz0Ceo8cjsfxFPIWAaAe2Q', compression='gzip')
-    cy2 = pd.read_pickle('https://drive.google.com/uc?id=17XfesauEIBESFvGdOgQg4neWxHTVRJYP', compression='gzip')
-    cy3 = pd.read_pickle('https://drive.google.com/uc?id=1ln9nPdZAI0d8dj2qa_w0Lc0Yma1HzqlS', compression='gzip')
-    cy4 = pd.read_pickle('https://drive.google.com/uc?id=1uv_Pt2bOYTMfoUWrnFfUErjJY7NIVy7S', compression='gzip')
+    cy1 = pd.read_pickle('../pkls/CY19.pkl', compression='gzip')
+    cy2 = pd.read_pickle('../pkls/CY20.pkl', compression='gzip')
+    cy3 = pd.read_pickle('../pkls/CY21.pkl', compression='gzip')
+    cy4 = pd.read_pickle('../pkls/CY22.pkl', compression='gzip')
     cy = pd.concat([cy1, cy2], ignore_index=True, axis=0)
     cy = pd.concat([cy, cy3], ignore_index=True, axis=0)
     cy = pd.concat([cy, cy4], ignore_index=True, axis=0)
--- dbs/db_revenue.py
+++ dbs/db_revenue.py
@@ -1,24 +1,22 @@
 import motor.motor_asyncio
 from models.model import Revenue
-from models.model import RevenueMaxDate
-
+import pymongo
+import pandas as pd
 
 #fetch all revenues
 def fetch_all_revenue(db:str):
-    client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://DataLog:DataLog@cluster0.jzr1zc7.mongodb.net/')
+    client = pymongo.MongoClient('mongodb+srv://DataLog:DataLog@cluster0.jzr1zc7.mongodb.net/')
     database = client[db]
     collection = database.df_sales
 
-    revenues = []
-    # cursor = collection.find({})
-    max_date = collection.find().sort([("Date",-1)]).limit(1)
-
+    max_date = collection.find({'$and':[{"Establishment":0}, {"Establishment":1}]})
+    
     for maxDate in max_date:
         maxDate
 
     max_month = int(maxDate["Date"][5:7])
     max_year = int(maxDate["Date"][0:4])
-   
+
     cursor = collection.aggregate([
         {
             "$project":
@@ -26,21 +24,23 @@ def fetch_all_revenue(db:str):
                     "year" :{"$year": { "$toDate": "$Date"}},
                     "month": {"$month": { "$toDate": "$Date"}},
                     "Date" : "$Date",
-                    "revenue" : "$dailyRevenue"
+                    "revenue" : "$dailyRevenue",
+                    'establishment': '$Establishment',
+                    "_id":0
                 }
         }
         ,
         {
             "$match" : { "month" :max_month, "year": max_year }
-        }
-
+        },
+        { '$sort': {'Date':1,  "establishment": 1 } },
     ])
 
-
-    for document in cursor:
-        revenues.append(RevenueMaxDate(**document))
-    return revenues
-
+    df = pd.DataFrame(cursor)
+    df = df.drop_duplicates()
+    df = df.to_dict(orient='records')
+    
+    return df
     
 #detch revenue by range
 def fetch_by_range_revenue(db, start_date,end_date):
