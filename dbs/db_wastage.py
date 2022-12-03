050085fb048a81d84ea7e94ef2d449039721e30c
ferryalvianto <ferryalvianto@gmail.com>
2022-12-02 23:24:52 -0800

    wastage api fixed

--- dbs/db_wastage.py
+++ dbs/db_wastage.py
@@ -1,9 +1,9 @@
 import motor.motor_asyncio
 
 async def fetch_all_wastage(db):
-    client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://DataLog:DataLog@cluster0.jzr1zc7.mongodb.net/test')
+    client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://DataLog:DataLog@cluster0.jzr1zc7.mongodb.net/')
     database = client[db]
-    collection = database.Wastage
+    collection = database.wastage
     wastages = []
     cursor = collection.aggregate([ 
                                     {'$group': {"_id" : "$Name", "Total_Quantity": {"$sum": "$Quantity"}   }},
@@ -15,15 +15,15 @@ async def fetch_all_wastage(db):
     return wastages
 
 async def fetch_date_range_wastage(db, start_date,end_date):
-    client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://DataLog:DataLog@cluster0.jzr1zc7.mongodb.net/test')
+    client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://DataLog:DataLog@cluster0.jzr1zc7.mongodb.net/')
     database = client[db]
-    collection = database.Wastage
+    collection = database.wastage
     wastages = []
     cursor = collection.aggregate([ {'$match': {'Date': { "$gte": start_date, "$lte":  end_date} }},
-                                    {'$group': {"_id" : "$Name", "Total_Quantity": {"$sum": "$Quantity"}   }},
+                                    {'$group': {"_id" : "$Name", "Total_Quantity": {"$sum": "$Quantity"} }},
                                     {'$sort': {"Total_Quantity": -1}},
-                                    {'$limit': 10}
-                                ])
+                                    {'$limit': 10},
+                                    ])
     async for document in cursor:
         wastages.append(document)
     return wastages
