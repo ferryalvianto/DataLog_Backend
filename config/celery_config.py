194f993953f3b9eff1416a948f23f32c91bba044
ferryalvianto <ferryalvianto@gmail.com>
2022-12-04 11:33:22 -0800

    uncomment localhost

--- config/celery_config.py
+++ config/celery_config.py
@@ -12,8 +12,8 @@ def route_task(name, args, kwargs, options, task=None, **kw):
 
 
 class BaseConfig:
-    CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
-    # broker_url: str = os.environ.get("CELERY_BROKER_URL", "amqps://yonaaaww:KJZVILlOh3aofNKPjbfm4u2hm9G1FiKt@shark.rmq.cloudamqp.com/yonaaaww")
+    # CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
+    broker_url: str = os.environ.get("CELERY_BROKER_URL", "amqps://yonaaaww:KJZVILlOh3aofNKPjbfm4u2hm9G1FiKt@shark.rmq.cloudamqp.com/yonaaaww")
     result_backend: str = os.environ.get("CELERY_RESULT_BACKEND", "rpc://")
 
     CELERY_TASK_QUEUES: list = (
